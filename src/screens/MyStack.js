05acd54dcf247879225c384330a2505086fecf3d
Felipe Fontoura <felipelfontoura@gmail.com>
2022-03-02 14:48:35 -0300

    initialCommit

--- src/screens/MyStack.js
+++ src/screens/MyStack.js
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './Login';
+import Home from './Home';
+import Details from './Details';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,8 +11,14 @@ const MyStack = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-      <Stack.Screen name="Login" component={Login}
+        <Stack.Screen name="Login" component={Login}
           options={{ title: 'Bem-vindo!' }}
+        />
+        <Stack.Screen name="Home" component={Home}
+          options={{ title: 'Aproveite a nossa seleção!' }}
+        />
+        <Stack.Screen name="Details" component={Details}
+          options={{ title: '' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
