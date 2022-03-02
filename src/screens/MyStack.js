b2a451c8e973830fa8f3c68001e268a52d3da283
Felipe Fontoura <felipelfontoura@gmail.com>
2022-03-02 18:32:18 -0300

    initialCommit

--- src/screens/MyStack.js
+++ src/screens/MyStack.js
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './Login';
 import Home from './Home';
 import Details from './Details';
